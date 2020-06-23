1536f5a55966fb2d6d7d70da93cac59889981101
Travis CI User <travis@example.org>
2020-06-23 14:44:53 +0000

    Updates deps, makes compliance links accessible by pendo (#163)

--- js/5.js
+++ js/5.js
@@ -1,2 +1,2 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{780:function(e,n){e.exports=function(e,n,o){return n in e?Object.defineProperty(e,n,{value:o,enumerable:!0,configurable:!0,writable:!0}):e[n]=o,e}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{779:function(e,n){e.exports=function(e,n,o){return n in e?Object.defineProperty(e,n,{value:o,enumerable:!0,configurable:!0,writable:!0}):e[n]=o,e}}}]);
 //# sourceMappingURL=5.js.map
