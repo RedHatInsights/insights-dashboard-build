a86adef8445d2e3c2189ff842677c79a33779e97
Travis CI User <travis@example.org>
2020-09-17 16:05:08 +0000

    Updates advisor card to match new mocks (#185)
    fixes https://issues.redhat.com/browse/ADVISOR-1300

---
+++ js/5.329d5a2e7abec9883b66.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{987:function(e,n){e.exports=function(e,n,o){return n in e?Object.defineProperty(e,n,{value:o,enumerable:!0,configurable:!0,writable:!0}):e[n]=o,e}}}]);
+//# sourceMappingURL=5.329d5a2e7abec9883b66.js.map
