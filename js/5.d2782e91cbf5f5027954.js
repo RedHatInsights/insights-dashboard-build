5b20f0607233ba1644ee2cfc74a71e0af005bac1
Travis CI User <travis@example.org>
2020-09-17 16:41:05 +0000

    If there are no incidents, advisor incident text is green (#186)

--- js/5.d2782e91cbf5f5027954.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{987:function(e,n){e.exports=function(e,n,o){return n in e?Object.defineProperty(e,n,{value:o,enumerable:!0,configurable:!0,writable:!0}):e[n]=o,e}}}]);
-//# sourceMappingURL=5.d2782e91cbf5f5027954.js.map
