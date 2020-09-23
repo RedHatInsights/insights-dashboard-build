b1b89935f26e146c1f6d2005c9f36f94ad6597da
Travis CI User <travis@example.org>
2020-09-23 17:51:17 +0000

    Adds workload to app state, advisor card to consume/use (#188)
    fixes https://issues.redhat.com/browse/ADVISOR-1364

--- js/5.d628319419476cecbb75.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{1008:function(e,n){e.exports=function(e,n,o){return n in e?Object.defineProperty(e,n,{value:o,enumerable:!0,configurable:!0,writable:!0}):e[n]=o,e}}}]);
-//# sourceMappingURL=5.d628319419476cecbb75.js.map
