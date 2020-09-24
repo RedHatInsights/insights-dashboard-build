43f9f0bb6d8ba3ec828a3e35bdc7f39b646229e9
Travis CI User <travis@example.org>
2020-09-24 20:09:24 +0000

    Advisor card to support insights and advisor rbac perms (#189)

--- js/5.8a44bfec68f05bf11d60.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{954:function(e,n){e.exports=function(e,n,o){return n in e?Object.defineProperty(e,n,{value:o,enumerable:!0,configurable:!0,writable:!0}):e[n]=o,e}}}]);
-//# sourceMappingURL=5.8a44bfec68f05bf11d60.js.map
