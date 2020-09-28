fadc168eb0d11fa89e66d32d3ac72ea878d70c54
Travis CI User <travis@example.org>
2020-09-28 16:42:14 +0000

    Ensure dashboard app loading state is wonderful ✅ (#190)
    fixes https://issues.redhat.com/browse/ADVISOR-668

--- js/5.ed84a89b56dae2d3d289.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{954:function(e,n){e.exports=function(e,n,o){return n in e?Object.defineProperty(e,n,{value:o,enumerable:!0,configurable:!0,writable:!0}):e[n]=o,e}}}]);
-//# sourceMappingURL=5.ed84a89b56dae2d3d289.js.map
