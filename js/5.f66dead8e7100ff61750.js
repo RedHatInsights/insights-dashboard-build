02e9685183cad543d3e3294f6aecffadc7789da0
Travis CI User <travis@example.org>
2020-09-29 20:15:23 +0000

    Add ternary operator for url creation in advisor card (#191)

---
+++ js/5.f66dead8e7100ff61750.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{862:function(e,n){e.exports=function(e,n,o){return n in e?Object.defineProperty(e,n,{value:o,enumerable:!0,configurable:!0,writable:!0}):e[n]=o,e}}}]);
+//# sourceMappingURL=5.f66dead8e7100ff61750.js.map
