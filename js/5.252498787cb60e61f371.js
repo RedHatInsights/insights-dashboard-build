6b617c67b56689933a96bc2b0f5c26f5d5aab5ba
Travis CI User <travis@example.org>
2020-10-07 19:29:29 +0000

    Cards that do not support workloads now indicate when they're selected 🤗 (#195)
    fixes https://issues.redhat.com/browse/ADVISOR-1395
    
    Update advisor pie to better handle 0 state

--- js/5.252498787cb60e61f371.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{865:function(e,n){e.exports=function(e,n,o){return n in e?Object.defineProperty(e,n,{value:o,enumerable:!0,configurable:!0,writable:!0}):e[n]=o,e}}}]);
-//# sourceMappingURL=5.252498787cb60e61f371.js.map
