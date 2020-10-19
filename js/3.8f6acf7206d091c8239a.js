8a0975032688dc8257a96fbb8580dde216cb4b87
Travis CI User <travis@example.org>
2020-10-19 15:58:13 +0000

    Patch card to support workloads/tagging/sids 🚤✅🥰 (#201)
    fixes https://issues.redhat.com/browse/ADVISOR-1405

--- js/3.8f6acf7206d091c8239a.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{806:function(e,n){e.exports=function(e,n,o){return n in e?Object.defineProperty(e,n,{value:o,enumerable:!0,configurable:!0,writable:!0}):e[n]=o,e}}}]);
-//# sourceMappingURL=3.8f6acf7206d091c8239a.js.map
