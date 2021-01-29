054c2c20f4d85198701d7c2de95cc2d36af2eaa4
Travis CI User <travis@example.org>
2021-01-29 16:09:43 +0000

    Speed up build (#223)

--- js/7722.9530add25bcc4edae557.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[7722],{59713:o=>{o.exports=function(o,t,e){return t in o?Object.defineProperty(o,t,{value:e,enumerable:!0,configurable:!0,writable:!0}):o[t]=e,o}},61160:o=>{function t(e){return"function"==typeof Symbol&&"symbol"==typeof Symbol.iterator?o.exports=t=function(o){return typeof o}:o.exports=t=function(o){return o&&"function"==typeof Symbol&&o.constructor===Symbol&&o!==Symbol.prototype?"symbol":typeof o},t(e)}o.exports=t}}]);
-//# sourceMappingURL=7722.9530add25bcc4edae557.js.map
---
+++ js/8446.b0c4ba825da3cb13b5dd.js
@@ -0,0 +1,2 @@
+(self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[8446],{18446:(s,a,h)=>{var n=h(90939);s.exports=function(s,a){return n(s,a)}}}]);
+//# sourceMappingURL=8446.b0c4ba825da3cb13b5dd.js.map
