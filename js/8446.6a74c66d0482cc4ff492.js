b22fe3fe3344bf940812b19a5990c19506ef89d4
Travis CI User <travis@example.org>
2021-04-12 12:28:05 +0000

    Update deps 🥑🥑🥳🥳💯🥸also, if we have no newrule node_id hide the kba link (#242)

--- js/8446.6a74c66d0482cc4ff492.js
+++ js/8446.6a74c66d0482cc4ff492.js
@@ -1,2 +1,1 @@
 (self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[8446],{18446:(s,a,h)=>{var n=h(90939);s.exports=function(s,a){return n(s,a)}}}]);
-//# sourceMappingURL=8446.6a74c66d0482cc4ff492.js.map
