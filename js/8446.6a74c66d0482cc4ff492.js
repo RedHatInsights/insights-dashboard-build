358af2900b16f2238def98d290452cb464110e92
Travis CI User <travis@example.org>
2021-04-27 13:46:27 +0000

    fix(dashboard): unbroke mobile layout (#248)

--- js/8446.6a74c66d0482cc4ff492.js
+++ js/8446.6a74c66d0482cc4ff492.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[8446],{18446:(s,a,h)=>{var n=h(90939);s.exports=function(s,a){return n(s,a)}}}]);
-//# sourceMappingURL=../sourcemaps/8446.7fb93e63a4ab00f6e8ac.js.map
+//# sourceMappingURL=../sourcemaps/8446.b310fc3effce97a14e6c.js.map
