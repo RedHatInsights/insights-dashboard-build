9db90821a19e3ada4b322abaeb4d68a4522583bc
Travis CI User <travis@example.org>
2021-04-27 13:46:18 +0000

    fix(dashboard): unbroke mobile layout (#248)

--- js/2906.dd52ec9d20b005ac9324.js
+++ js/2906.dd52ec9d20b005ac9324.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[2906],{22122:(r,t,n)=>{"use strict";function e(){return(e=Object.assign||function(r){for(var t=1;t<arguments.length;t++){var n=arguments[t];for(var e in n)Object.prototype.hasOwnProperty.call(n,e)&&(r[e]=n[e])}return r}).apply(this,arguments)}n.d(t,{Z:()=>e})},19756:(r,t,n)=>{"use strict";function e(r,t){if(null==r)return{};var n,e,s={},a=Object.keys(r);for(e=0;e<a.length;e++)n=a[e],t.indexOf(n)>=0||(s[n]=r[n]);return s}n.d(t,{Z:()=>e})}}]);
-//# sourceMappingURL=../sourcemaps/2906.7fb93e63a4ab00f6e8ac.js.map
+//# sourceMappingURL=../sourcemaps/2906.b310fc3effce97a14e6c.js.map
