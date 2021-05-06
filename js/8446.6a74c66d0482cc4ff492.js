ec436e543534acdfa5bb81102a84972d7066bd32
Travis CI User <travis@example.org>
2021-05-06 14:29:09 +0000

    Fix security url, removes insights base (#250)
    fixes https://issues.redhat.com/browse/ADVISOR-1724

--- js/8446.6a74c66d0482cc4ff492.js
+++ js/8446.6a74c66d0482cc4ff492.js
@@ -1,2 +1,2 @@
 (self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[8446],{18446:(s,a,h)=>{var n=h(90939);s.exports=function(s,a){return n(s,a)}}}]);
-//# sourceMappingURL=../sourcemaps/8446.b40bf7574ef961b5b21f.js.map
+//# sourceMappingURL=../sourcemaps/8446.49982bffd62c35d4ef26.js.map
