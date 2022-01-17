06b09b002104ef74d77ee13461710c3cdfa940a4
Travis CI User <travis@example.org>
2022-01-17 11:42:48 +0000

    [stable] Deploy new build (#283)
    * fix(ComplianceCard): RHICOMPL-1026 - Minor 💅
    
    * fix url to Patch UI
    
    * further code to fix URL to Patch
    
    * fix(ComplianceCard): RHICOMPL-1733 - Correct link to create policy
    
    * Added client-side routing to insights-dashboard
    
    * RHIROS-36: add Resource Optimization card on dashboard (#273)
    
    * added basic code for showing ros card on dashboard
    
    * RHIROS-36 added is_configured api call to fetch details for ROS card
    
    * added logic to handle API response and missing translations
    
    * fixed lint errors
    
    * Use webpack proxy at all times and remove html webpack plugin (#274)
    
    * fix(ComplianceCardEpmtyState): RHICOMPL-2434 - Correct empty state title (#279)
    
    * Use chrome HTML to fix failing ESI tag mutations. (#281)
    
    Co-authored-by: Karel Hala <khala@redhat.com>
    
    Co-authored-by: Sebastian Gräßl <mail@bastilian.me>
    Co-authored-by: mkholjur <mkholjur@redhat.com>
    Co-authored-by: Richard Všianský <32869456+rvsia@users.noreply.github.com>
    Co-authored-by: Radek Kalužík <radek.kaluzik@email.cz>
    Co-authored-by: Preeti Wadhwani <pwadhwani2592@gmail.com>
    Co-authored-by: Karel Hala <khala@redhat.com>
    Co-authored-by: Egor Shamardin <33912805+LightOfHeaven1994@users.noreply.github.com>

---
+++ js/434.1642419685415.6421d23c5e1baba52886.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[434],{32857:()=>{},30187:()=>{},90479:()=>{},78752:()=>{},21626:()=>{},54994:()=>{},37494:()=>{},67761:()=>{}}]);
