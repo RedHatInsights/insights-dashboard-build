2237539cf96d4b8d4e1ed5d4f66179ad0a7dd28e
Travis CI User <travis@example.org>
2022-10-13 08:45:18 +0000

    fix(counts): use puptoo as a filter attribute instead insights

---
+++ js/1027.1665650646433.3c038d9b7a717cd963d2.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[1027],{91027:()=>{}}]);
---
+++ js/3661.1665650646433.3c038d9b7a717cd963d2.js
@@ -0,0 +1,1 @@
+(self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[3661],{93661:()=>{}}]);
