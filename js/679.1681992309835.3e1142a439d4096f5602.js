e9dffce2578a5ec1aa8c2999d7a8093229daf7c9
Travis CI User <travis@example.org>
2023-04-20 14:25:13 +0000

    Merge pull request #362 from adonispuente/zerostate
    feat(ZeroState): New ZeroState for apps: functionality and most styling

--- js/679.1681992309835.3e1142a439d4096f5602.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[679],{28412:(e,t,r)=>{r.r(t),r.d(t,{default:()=>i});var n=r(20333),s=r(87905),a=r(28216),l=r(93264),o=r.n(l),c=r(75662),u=r(29095),g=r(83130);const i=function(){return o().createElement(s.Z,{locale:navigator.language.slice(0,2),messages:g,onError:console.log},o().createElement(a.zt,{store:(0,u.S1)().getStore()},o().createElement(c.BrowserRouter,null,o().createElement(n.Z,null))))}}}]);
-//# sourceMappingURL=../sourcemaps/679.eecc2ae5f472d8a6b8209087caf5a1d7.js.map
