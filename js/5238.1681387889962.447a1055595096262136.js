4b1ef248961009dca1d08c483dd6f562aa3ab3d0
Travis CI User <travis@example.org>
2023-05-02 19:48:18 +0000

    Revert "feat(ESSNTL-4496): Replace SmartManager w/ satellite"
    This reverts commit 40d425f1f8b2926281282d4e2aac3132549221fe.

--- js/5238.1681387889962.447a1055595096262136.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[5238],{25238:()=>{}}]);
--- js/7350.1681387889962.447a1055595096262136.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunkinsights_dashboard=self.webpackChunkinsights_dashboard||[]).push([[7350],{77350:()=>{}}]);
